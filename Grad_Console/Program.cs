2bffcef99f9f5b618fbd8cbc1838709e4fbdf57b
Webessential@outlook.sa <Webessential@outlook.sa>
2022-09-06 13:03:15 +0300

    Add project files.

---
+++ Grad_Console/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] A = new int[10] { 10, 4, 5, 6, 22, 3, 7, 8, 9, 10 };
+string[] B = { "GTC", "Book", "Cat", "X", "Door" };
+
+double avg = A.Average();
+
+int sum = A.Sum();
+int min = A.Min();
+
+int max = A.Max();
+
+
+Console.WriteLine("SUM = {0:F}", sum);
+Console.WriteLine("Average = {0:F}", avg);
+Console.WriteLine("Min = {0:F}", min);
+Console.WriteLine("MAx = {0:F}", max);
+
+Console.Write("Enter value to search:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+bool isexists = A.Contains(x);
+Console.WriteLine(" the search result is {0}", isexists);
+
+int pos = Array.IndexOf(A, x);
+Console.WriteLine(" the {0} found in  {1} position", x, pos);
+
+
+B.Reverse();
+Array.Sort(B);
+
+
+Console.WriteLine();
+foreach (string item in B)
+    Console.WriteLine(item);
+
+Console.ReadKey();
