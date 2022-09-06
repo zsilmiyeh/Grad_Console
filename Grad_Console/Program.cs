42cc1b943ce3bfc7b8b75b4c2573458e4a22b57a
Webessential@outlook.sa <Webessential@outlook.sa>
2022-09-06 13:16:27 +0300

    add Grad book class

--- Grad_Console/Program.cs
+++ Grad_Console/Program.cs
@@ -1,37 +1,50 @@
 ﻿// this an introduction to array class in C#
 
-int[] A = new int[10] { 10, 4, 5, 6, 22, 3, 7, 8, 9, 10,12,15 };
-string[] B = { "GTC", "Book", "Cat", "X", "Door" };
+ //static void Main()
+{
+    int[] A = new int[] { 10, 4, 5, 6, 22, 3, 7, 8, 9, 10, 12, 15 };
+    string[] B = { "GTC", "Book", "Cat", "X", "Door" };
 
-double avg = A.Average();
+    double avg = A.Average();
 
-int sum = A.Sum();
-int min = A.Min();
+    int sum = A.Sum();
+    int min = A.Min();
 
-int max = A.Max();
-
-
-Console.WriteLine("SUM = {0:F}", sum);
-Console.WriteLine("Average = {0:F}", avg);
-Console.WriteLine("Min = {0:F}", min);
-Console.WriteLine("MAx = {0:F}", max);
-
-Console.Write("Enter value to search:");
-int x = Convert.ToInt32(Console.ReadLine());
-
-bool isexists = A.Contains(x);
-Console.WriteLine(" the search result is {0}", isexists);
-
-int pos = Array.IndexOf(A, x);
-Console.WriteLine(" the {0} found in  {1} position", x, pos);
+    int max = A.Max();
 
 
-B.Reverse();
-Array.Sort(B);
+    Console.WriteLine("SUM = {0:F}", sum);
+    Console.WriteLine("Average = {0:F}", avg);
+    Console.WriteLine("Min = {0:F}", min);
+    Console.WriteLine("MAx = {0:F}", max);
+
+    Console.Write("Enter value to search:");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    bool isexists = A.Contains(x);
+    Console.WriteLine(" the search result is {0}", isexists);
+
+    int pos = Array.IndexOf(A, x);
+    Console.WriteLine(" the {0} found in  {1} position", x, pos);
 
 
-Console.WriteLine();
-foreach (string item in B)
-    Console.WriteLine(item);
+    B.Reverse();
+    Array.Sort(B);
 
-Console.ReadKey();
+
+    Console.WriteLine();
+    foreach (string item in B)
+        Console.WriteLine(item);
+
+
+
+    // one-dimensional array of student grades
+   
+    var myGradeBook = new GradeBook.GradeBook("CS101 Introduction to C# Programming", A);
+    myGradeBook.DisplayMessage();
+    myGradeBook.ProcessGrades();
+
+
+    Console.ReadKey();
+
+}
